cf76a30e39b8284e940f8f1b2475a70e690270d0
lamkostal <lamkostal@gmail.com>
2020-07-18 23:56:45 +0300

    adds tablericons rmv fa-svelte

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,11 +1,6 @@
 <script>
     export let title,description,src,downLink,free
-    import Icon from 'fa-svelte'
-    import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload'
-    import { faComments } from '@fortawesome/free-solid-svg-icons/faComments'
-    
-    let downIcon=faDownload
-    let speachIcon=faComments
+   
     
     import {createEventDispatcher} from 'svelte';
     const dispatch = createEventDispatcher();
@@ -26,12 +21,34 @@
              <span>{description}</span>
     
             <div class="buttons_wrap" >
-                {#if free}<span><a href="{downLink}" download target="_blank" rel="noopener" on:click|stopPropagation="{openModal}">
-                    <Icon class="my-icon" icon={downIcon}></Icon> 
-                </a>Download video file</span>{/if}
-                <span><a href="#contact">
-                    <Icon class="my-icon"  icon={speachIcon} scale="3"></Icon>
-                </a> Contact for custom solution</span>
+                {#if free}
+                <span class="card-text-block">
+                    <span>Download video file</span>
+                    <a href="{downLink}" download target="_blank" rel="noopener" on:click|stopPropagation="{openModal}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="my-icon icon icon--download" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#00a870" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z"/>
+                        <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+                        <polyline points="7 11 12 16 17 11" />
+                        <line x1="12" y1="4" x2="12" y2="16" />
+                        </svg>
+                    <!-- <Icon class="my-icon" icon={downIcon}></Icon>  -->
+                    </a>
+                    
+                </span>
+                {/if}
+                <span class="card-text-block" >
+                    <span>Contact for customisation </span>
+                    <a href="#contact">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="my-icon icon icon--contact" width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#00a870" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z"/>
+                        <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1" />
+                        <line x1="12" y1="12" x2="12" y2="12.01" />
+                        <line x1="8" y1="12" x2="8" y2="12.01" />
+                        <line x1="16" y1="12" x2="16" y2="12.01" />
+                      </svg>
+                    <!-- <Icon class="my-icon"  icon={speachIcon} scale="3"></Icon> -->
+                </a> 
+                </span>
              
             </div>
          </div>
@@ -66,6 +83,24 @@
     .card a{
         color:var(--main-color);
     }
+    .card-text-block{
+        display:flex;
+        align-items: center;
+    }
+    .buttons_wrap{
+        display:flex;
+        justify-content: center;
+    }
+    @media (max-width:740px){
+        .card-text-block{
+        flex-direction: column;
+        flex-flow: column-reverse;
+    }
+    .buttons_wrap{
+        flex-direction: column;
+    }
+
+    }
     .thumb img{
         max-width:100%;
     }
@@ -87,17 +122,14 @@
     .description>*{
         margin:10px;
     }
-    .buttons_wrap{
-        display:flex;
-        justify-content: center;
-    }
+   
     .buttons_wrap>*{
         margin:15px;
     }
     a :global(.my-icon){
-        width:30px;
-        height:20px;
-        margin: 0 0px 0 5px ;
+        width:25px;
+        height:25px;
+        margin: 0 15px 0 5px ;
         text-decoration: none;
     }
     a:hover{
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,24 +1,34 @@
 <script>
-    import Icon from 'fa-svelte';
-    import { faComments } from '@fortawesome/free-solid-svg-icons/faComments';
-    import { faCameraRetro } from '@fortawesome/free-solid-svg-icons/faCameraRetro';
+  
 
 
-let galleryIcon=faCameraRetro
-let speachIcon=faComments
 </script>
 
 <nav class="navbar">
    <div class="logo-wrap"> <h1 class="temp">assetmio</h1></div>
     <ul>
         <li class="nav-item"><a href="#gallery">
-            <Icon icon={galleryIcon}/>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon  icon--photo" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.7" stroke="rgb(0, 168, 112)" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z"/>
+                <line x1="15" y1="8" x2="15.01" y2="8" />
+                <rect x="4" y="4" width="16" height="16" rx="3" />
+                <path d="M4 15l4 -4a3 5 0 0 1 3 0l 5 5" />
+                <path d="M14 14l1 -1a3 5 0 0 1 3 0l 2 2" />
+              </svg>
+          
             <span>gallery</span></a> 
         </li>
 
         <li class="nav-item">
             <a href="#contact">
-            <Icon icon={speachIcon}/>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon--contact " width="28" height="28" viewBox="0 0 24 24" stroke-width="2" stroke="#00a870" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z"/>
+                    <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1" />
+                    <line x1="12" y1="12" x2="12" y2="12.01" />
+                    <line x1="8" y1="12" x2="8" y2="12.01" />
+                    <line x1="16" y1="12" x2="16" y2="12.01" />
+                  </svg>
+          
             <span>contact</span></a> 
          </li>
     </ul>
