d00b939a959749477e0c9e71d0fa6bb9275500f8
lamkostal <lamkostal@gmail.com>
2020-07-30 15:59:46 +0300

    defers ccs-doodle,styli adds hey

--- src/sources.js
+++ src/sources.js
@@ -1,171 +1,173 @@
-export let sources = [
-    {
-      title: "Lamkos",
-      descr: {
-        size: "",
-        duration: "",
-        format: "",
-      },
-      downLink: "/mp4/lamkos.mp4",
-      downloadFree: false,
-      seamless: false,
+export let sources = [{
+    title: "Lamkos",
+    descr: {
+      size: "",
+      duration: "",
+      format: "",
     },
-    {
-      title: "polyconnect",
-      descr: {
-        size: "1920X1080 (1.08mb) ",
-        duration: "4s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/polyconnect.mp4",
-      downloadFree: true,
-      seamless: true,
+    downLink: "/mp4/lamkos.mp4",
+    downloadFree: false,
+    seamless: false,
+  },
+  {
+    title: "polyconnect",
+    descr: {
+      size: "1920X1080 (1.08mb) ",
+      duration: "4s",
+      format: "25fps mp4",
     },
-    {
-     
-      title: "electrons",
-      descr: {
-        size: "",
-        duration: "",
-        format: "",
-      },
-      downLink: "/mp4/electrons.mp4",
-      downloadFree: false,
-      seamless: false,
-    },
-    {
-     
-      title: "connect1",
-      descr: {
-        size: "1920X1080 (1.03mb) ",
-        duration: "3s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/connect1.mp4",
-      downloadFree: true,
-      seamless: true,
-    },
-    {
-      
-      title: "jeskai-ink",
-      descr: {
-        size: "4:055s 30fps mp4",
-        duration: "",
-        format: "",
-        
-      },
-      downLink: "/mp4/jeskai768.mp4",
-      downloadFree: false,
-      seamless: false,
-    },
+    downLink: "/mp4/polyconnect.mp4",
+    downloadFree: true,
+    seamless: true,
+  },
+  {
 
-    {
-     
-      title: "wavy-blobs",
-      descr: {
-        size: "1920X1080 (0.35mb) ",
-        duration: "4s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/wavy-blobs.mp4",
-      downloadFree: true,
-      seamless: true,
+    title: "electrons",
+    descr: {
+      size: "",
+      duration: "",
+      format: "",
     },
-    {
-      
-      title: "Constellation",
-      descr: {
-        size: "1080x1920 (0.7mb) ",
-        duration: "4s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/constell.mp4",
-      downloadFree: true,
-      seamless: true,
+    downLink: "/mp4/electrons.mp4",
+    downloadFree: false,
+    seamless: false,
+  },
+  {
+
+    title: "connect1",
+    descr: {
+      size: "1920X1080 (1.03mb) ",
+      duration: "3s",
+      format: "25fps mp4",
     },
-    {
-     
-      title: "Bokeh-green",
-      descr: {
-        size: "1080x1920 (0.85mb) ",
-        duration: "5s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/bokeh-green.mp4",
-      downloadFree: true,
-      seamless: true,
+    downLink: "/mp4/connect1.mp4",
+    downloadFree: true,
+    seamless: true,
+  },
+  {
+
+    title: "jeskai-ink",
+    descr: {
+      size: "4:055s 30fps mp4",
+      duration: "",
+      format: "",
+
     },
-    {
-     
-      title: "Bokeh-red",
-      descr: {
-        size: "1080x1920 (0.7mb)",
-        duration: "4s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/bokeh-red.mp4",
-      downloadFree: true,
-      seamless: true,
+    downLink: "/mp4/jeskai768.mp4",
+    downloadFree: false,
+    seamless: false,
+  },
+
+  {
+
+    title: "wavy-blobs",
+    descr: {
+      size: "1920X1080 (0.35mb) ",
+      duration: "4s",
+      format: "25fps mp4",
     },
-    {
-     
-      title: "PsycSplash",
-      descr: {
-        size: "1080x1920 (2.13mb)",
-        duration: "10s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/psychSplash.mp4",
-      downloadFree: true,
-      seamless: true,
+    downLink: "/mp4/wavy-blobs.mp4",
+    downloadFree: true,
+    seamless: true,
+  },
+  {
+    title: "Constellation",
+    descr: {
+      size: "1080x1920 (0.7mb) ",
+      duration: "4s",
+      format: "25fps mp4",
     },
-     {
-     
-      title: "Oceanic",
-      descr: {
-        size: "1080x1920 (1.7mb)",
-        duration: "5s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/oceanic.mp4",
-      downloadFree: false,
-      seamless: false,
+    downLink: "/mp4/constell.mp4",
+    downloadFree: true,
+    seamless: true,
+  },
+  {
+    title: "Bokeh-green",
+    descr: {
+      size: "1080x1920 (0.85mb) ",
+      duration: "5s",
+      format: "25fps mp4",
     },
-     {
-     
-      title: "Glitchy",
-      descr: {
-        size: "",
-        duration: "",
-      },
-      downLink: "/mp4/glitchy.mp4",
-      downloadFree: false,
-      seamless: true,
+    downLink: "/mp4/bokeh-green.mp4",
+    downloadFree: true,
+    seamless: true,
+  },
+  {
+    title: "Bokeh-red",
+    descr: {
+      size: "1080x1920 (0.7mb)",
+      duration: "4s",
+      format: "25fps mp4",
     },
-     {
-    
-      title: "Cubic",
-      descr: {
-        size: "",
-        duration: "",
-        format: "",
-      },
-      downLink: "/mp4/cubic.mp4",
-      downloadFree: false,
-      seamless: false,
+    downLink: "/mp4/bokeh-red.mp4",
+    downloadFree: true,
+    seamless: true,
+  },
+  {
+    title: "PsycSplash",
+    descr: {
+      size: "1080x1920 (2.13mb)",
+      duration: "10s",
+      format: "25fps mp4",
     },
-    {
-      
-      title: "Countdown",
-      descr: {
-        size: "1080x1920 (0.4mb)",
-        duration: "4s",
-        format: "25fps mp4",
-      },
-      downLink: "/mp4/countdown.mp4",
-      downloadFree: true,
-      seamless: false,
+    downLink: "/mp4/psychSplash.mp4",
+    downloadFree: true,
+    seamless: true,
+  },
+  {
+    title: "Oceanic",
+    descr: {
+      size: "1080x1920 (1.7mb)",
+      duration: "5s",
+      format: "25fps mp4",
     },
+    downLink: "/mp4/oceanic.mp4",
+    downloadFree: false,
+    seamless: false,
+  },
+  {
+    title: "Glitchy",
+    descr: {
+      size: "",
+      duration: "",
+    },
+    downLink: "/mp4/glitchy.mp4",
+    downloadFree: false,
+    seamless: true,
+  },
+  {
+    title: "Cubic",
+    descr: {
+      size: "",
+      duration: "",
+      format: "",
+    },
+    downLink: "/mp4/cubic.mp4",
+    downloadFree: false,
+    seamless: false,
+  },
+  {
+    title: "Countdown",
+    descr: {
+      size: "1080x1920 (0.4mb)",
+      duration: "4s",
+      format: "25fps mp4",
+    },
+    downLink: "/mp4/countdown.mp4",
+    downloadFree: true,
+    seamless: false,
+  },
+  {
+    title: "Hey",
+    descr: {
+      size: "1080x1920 (0.4mb)",
+      duration: "4s",
+      format: "25fps mp4",
+    },
+    downLink: "/mp4/hey.mp4",
+    downloadFree: false,
+    seamless: false,
+  },
 
 
-  ];
+];
