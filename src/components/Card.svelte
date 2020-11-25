9fae500bee4353c87cebe39fb7f50fe9b6abf2bb
lamkostal <lamkostal@gmail.com>
2020-11-25 16:07:12 +0200

    add stream6, add padding to card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -116,7 +116,7 @@
     .card{
         overflow: hidden;
         position:relative;
-        padding:1.5rem 2rem 0.5rem;
+        padding:2.5rem 3rem 1.5rem;
         background-color: #fff;
         display:flex;
         flex-direction:column;
