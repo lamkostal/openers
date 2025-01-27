2e0f06221a659c336719938b8916d999883e793b
lamkostal <lamkostal@gmail.com>
2025-01-27 12:42:02 +0200

    added  by kd in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 
 <footer>
     <div class="footer-wrap">
-        <span>created with </span>
+        <span>created by <a href="https://kleo-dimi.gr/works">KD</a> with </span>
  <i>
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="15" height="15" viewBox="0 0 24 24" stroke-width="1.8" stroke="#F44336" fill="none" stroke-linecap="round" stroke-linejoin="round">
   <path stroke="none" d="M0 0h24v24H0z"/>
